748cf752e374eda207d1f8a6250fe50a1d2f585f
disinvite <disinvite@users.noreply.github.com>
2018-10-23 16:44:17 -0400

    ugh

--- xyz.js
+++ xyz.js
@@ -11,6 +11,8 @@ const flags = {
   negative: 0
 };
 
+// these aren't the right reset flags
+
 let pc = 0xc000;
 
 const rom = fs.readFileSync('./roms/nestest.nes');
@@ -20,9 +22,25 @@ console.log(header);
 const prg = rom.slice(16,16384 + 16);
 console.log(prg);
 
+function prg_read(addr) {
+  return prg.readUInt8(addr - 0xc000); // ugh
+}
+
+function prg_read_16(addr) {
+  return prg.readUInt16BE(addr - 0xc000); // ugh
+}
+
 for(let _x = 0; _x < 5; _x++) {
-  let b = prg.readUInt8(pc - 0xc000); // ugh
-  console.log(b);
-  console.log(opcodes[b]);
+  let b = prg_read(pc); // ugh
+  let opcode = opcodes[b];
+  let address;
+  switch(opcode.mode) {
+    case 'Absolute':
+      address = prg_read_16(pc + 1);
+      break;
+  }
+
+  console.log(address.toString(16));
+
 }
 
